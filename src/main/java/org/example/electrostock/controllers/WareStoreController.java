b7f378c13f10bf471de10a7ab5273fb8899b8ed9
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2025-04-04 16:49:10 +0300

    implement warestore edit & get all

--- src/main/java/org/example/electrostock/controllers/WareStoreController.java
+++ src/main/java/org/example/electrostock/controllers/WareStoreController.java
@@ -3,13 +3,18 @@ package org.example.electrostock.controllers;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.example.electrostock.dto.warestore.WareStoreCreateDto;
+import org.example.electrostock.dto.warestore.WareStoreEditDto;
 import org.example.electrostock.dto.warestore.WareStoreItemDto;
 import org.example.electrostock.entities.WareStoreEntity;
 import org.example.electrostock.mapper.WareStoreMapper;
 import org.example.electrostock.repositories.WareStoreRepository;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @CrossOrigin(origins = "http://localhost:5173")
 @RestController
@@ -19,6 +24,15 @@ import org.springframework.web.bind.annotation.*;
 public class WareStoreController {
     private final WareStoreRepository wareStoreRepository;
     private final WareStoreMapper wareStoreMapper;
+
+    @GetMapping
+    public ResponseEntity<List<WareStoreItemDto>> index() {
+        List<WareStoreItemDto> wareStores = wareStoreRepository.findAll()
+                .stream()
+                .map(wareStoreMapper::wareStoreItemDto)
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(wareStores, HttpStatus.OK);
+    }
 
     @PostMapping("create")
     public ResponseEntity<WareStoreItemDto> create(@RequestBody WareStoreCreateDto dto) {
@@ -33,4 +47,21 @@ public class WareStoreController {
         }
     }
 
+    @PutMapping("edit")
+    public ResponseEntity<WareStoreItemDto> edit(@RequestBody WareStoreEditDto dto) {
+        if (dto == null) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+
+        try {
+            WareStoreEntity entity = wareStoreMapper.wareStoreEditDto(dto);
+            wareStoreRepository.save(entity);
+            WareStoreItemDto responseDto = wareStoreMapper.wareStoreItemDto(entity);
+            return new ResponseEntity<>(responseDto, HttpStatus.OK);
+        } catch (DataIntegrityViolationException ex) {
+            return new ResponseEntity<>(null, HttpStatus.CONFLICT);
+        } catch (Exception ex) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- src/main/java/org/example/electrostock/mapper/WareStoreMapper.java
+++ src/main/java/org/example/electrostock/mapper/WareStoreMapper.java
@@ -1,11 +1,10 @@
 package org.example.electrostock.mapper;
 
 import org.example.electrostock.dto.warestore.WareStoreCreateDto;
+import org.example.electrostock.dto.warestore.WareStoreEditDto;
 import org.example.electrostock.dto.warestore.WareStoreItemDto;
 import org.example.electrostock.entities.WareStoreEntity;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 
 import java.util.List;
 
@@ -14,4 +13,5 @@ public interface WareStoreMapper {
     WareStoreItemDto wareStoreItemDto(WareStoreEntity entity);
     List<WareStoreItemDto> wareStoreEntitiesToDtos(List<WareStoreEntity> entities);
     WareStoreEntity wareStoreCreateDto(WareStoreCreateDto dto);
+    WareStoreEntity wareStoreEditDto(WareStoreEditDto dto);
 }
