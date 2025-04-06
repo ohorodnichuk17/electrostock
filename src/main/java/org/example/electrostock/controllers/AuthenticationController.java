ca99c28d5bb7bec0698d495166c7f9de72d57a34
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2025-04-06 13:08:55 +0300

    refactor code

--- src/main/java/org/example/electrostock/controllers/AuthenticationController.java
+++ src/main/java/org/example/electrostock/controllers/AuthenticationController.java
@@ -13,7 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("api/authentication")
 @RequiredArgsConstructor
